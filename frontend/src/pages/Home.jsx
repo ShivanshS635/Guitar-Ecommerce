0aa855266ee805c601abdbadfa5bf4e0cfb77d3c
Shivansh <shivansh832.be22@chitkara.edu.in>
2025-04-18 22:09:10 +0530

    90% done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import Hero from '../components/home/Hero.jsx'
 import CategoryHighlights from '../components/home/CategoryHighlights.jsx'
 import Craftsmanship from '../components/home/Craftsmanship.jsx'
-import FeaturedProducts from '../components/home/FeaturedProducts.jsx'
 import YouTubeReviews from '../components/home/YouTubeReviews.jsx'
 import ScreenshotGallery from '../components/home/ScreenshotGallery.jsx';
 import Testimonials from '../components/home/Testimonials.jsx';
-import ContactCTA from '../components/home/ContactCTA.jsx';
 import LatestProducts from '../components/LatestCategories.jsx'
 
 const Home = () => (
